535e362459c2aae6c3b13b60b6f9a662dde6ee99
Cole Davis <dylcdav@gmail.com>
2025-12-30 20:27:58 -0500

    Remove click listener

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Notice, Plugin } from 'obsidian';
+import { Plugin } from 'obsidian';
 import { DEFAULT_SETTINGS, AnkiLinkSettings, AnkiLinkSettingTab } from "./settings";
 
 export default class AnkiLink extends Plugin {
@@ -9,12 +9,6 @@ export default class AnkiLink extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new AnkiLinkSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
 	}
 
 	onunload() {
